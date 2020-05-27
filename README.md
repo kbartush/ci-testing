35cbf31ea781ff48b6a42391212b3ef70abba4bf
Kyle Bartush <kbartush@kochava.com>
2020-05-26 17:28:51 -0700

    asdf

---
+++ README.md
@@ -0,0 +1,2 @@
+# CI Testing
+[![CircleCI](https://circleci.com/gh/kbartush/ci-testing.svg?style=svg)](https://circleci.com/gh/kbartush/ci-testing)
---
+++ test/test.js
@@ -0,0 +1,9 @@
+/* eslint no-unused-expressions: 0 */
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+
+describe('test', () => {
+  it('test test', () => {
+    expect(!!(1 || 1)).to.be.true
+  })
+})
